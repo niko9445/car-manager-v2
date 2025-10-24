f93a9c36faba83b0bc2a92a38ff854ed12f7c860
niko9445 <karosta94@gmail.com>
2025-10-25 01:43:27 +0300

    полностью изменил стили

--- src/components/modals/AddCarModal/AddCarModal.tsx
+++ src/components/modals/AddCarModal/AddCarModal.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useMemo } from 'react';
+import React, { useState } from 'react';
 import Modal from '../../ui/Modal/Modal';
 import { CarFormData } from '../../../types';
 import { carBrands, engineTypes, transmissionTypes } from '../../../data/carBrands';
